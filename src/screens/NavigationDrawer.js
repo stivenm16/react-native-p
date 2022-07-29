6f6075265af8f45bcced4bcd5bf546ca5eef0d38
Kevin <stiivenmendez16@gmail.com>
2022-07-29 12:54:11 -0500

    update babel config to use drawer

---
+++ src/screens/NavigationDrawer.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+// import {createDr}
+
+
+
+export default function NavigationDrawer() {
+  return (
+    <View>
+      <Text>NavigationDrawer</Text>
+    </View>
+  )
+}
